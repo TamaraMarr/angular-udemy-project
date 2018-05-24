5b59ac6098b23e3b93ad46a9c529c679ae7c821e
TamaraMarr <tasha.m.bg@gmail.com>
2018-05-25 00:06:58 +0200

    Set component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Header } from './header/header.component';
 import { AppComponent } from './app.component';
+import { Footer } from './footer/footer.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { ShoppingListEdit } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeList } from './recipes/recipe-list/recipe-list.component';
+import { Recipes } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    Header,
+    AppComponent,
+    Footer,
+    ShoppingList,
+    ShoppingListEdit,
+    Recipes,
+    RecipeList
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
